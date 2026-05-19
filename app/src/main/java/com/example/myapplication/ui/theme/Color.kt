160fca9e292a46de8860b29afdd1942baaa7f46c
caleottw <caleottw@student.21-school.ru>
2026-05-19 14:31:48 +0500

    1

---
+++ app/src/main/java/com/example/myapplication/ui/theme/Color.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val WarmPrimary80 = Color(0xFFFFB4A9)
+val WarmSecondary80 = Color(0xFFE7BDB7)
+val WarmTertiary80 = Color(0xFFE4C54A)
+
+val WarmPrimary40 = Color(0xFFA9372A)
+val WarmSecondary40 = Color(0xFF775652)
+val WarmTertiary40 = Color(0xFF705D00)
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
